8a2e27e343586e4f3658d3205b1c642225fa8032
japhni <digital.hightechnology1993@gmail.com>
2024-12-20 20:24:28 +0200

    commit after removing .env and change something of .properties file

--- src/main/java/com/japhni81/jani/security/AppConfig.java
+++ src/main/java/com/japhni81/jani/security/AppConfig.java
@@ -1,14 +1,14 @@
 package com.japhni81.jani.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.FileSystemResource;
 
-@Configuration
+//@Configuration
 public class AppConfig {
     
-    @Bean
+//    @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
         configurer.setLocation(new FileSystemResource("./.env"));
